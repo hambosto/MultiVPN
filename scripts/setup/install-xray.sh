e4446fe17c05552f97d8cc93f9ea922995996294
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-02-28 12:32:31 +0700

    Update install-xray.sh

--- scripts/setup/install-xray.sh
+++ scripts/setup/install-xray.sh
@@ -2,52 +2,58 @@
 
 # Function to install essential packages and utilities
 install_essentials() {
-    apt update -y
-    apt upgrade -y
-    apt install socat jq apt-transport-https dnsutils -y
-    apt install cron ntpdate -y
-    ntpdate pool.ntp.org
-    apt install chrony -y
+    # Update and upgrade the system
+    apt update -y && apt upgrade -y
+
+    # Install essential packages
+    apt install -y socat python2 curl wget sed nano python3 jq cron bash-completion ntpdate chrony zip pwgen openssl netcat
+
+    # Configure and start chrony
     timedatectl set-ntp true
-    systemctl enable chronyd && systemctl restart chronyd
+    systemctl enable --now chronyd
+
+    # Set the timezone
     timedatectl set-timezone Asia/Jakarta
+
+    # Display chrony information
     chronyc sourcestats -v
     chronyc tracking -v
-    apt install zip -y
+    date
 }
+
 
 # Function to install XRAY Core
 install_xray_core() {
-    echo -e "=== Installing XRAY Core ==="
     mkdir -p /var/log/xray
-    chmod +x /var/log/xray
+    chmod x /var/log/xray
     mkdir -p /usr/local/etc/xray
-    latest_version=$(curl -sI "https://github.com/XTLS/Xray-core/releases/latest" | grep -i location | awk -F '/' '{print $NF}' | tr -d '\r\n[:space:]')
+    latest_version=$(curl -sI "https://github.com/XTLS/Xray-core/releases/latest" | grep -i location | awk -F '/' '{print $NF}' | tr -d 'rn[:space:]')
     xraycore_link="https://github.com/XTLS/Xray-core/releases/download/$latest_version/xray-linux-64.zip"
     cd "$(mktemp -d)"
     curl -sL "$xraycore_link" -o xray.zip
     unzip -q xray.zip && rm -rf xray.zip
     mv xray /usr/local/bin/xray
-    chmod +x /usr/local/bin/xray
+    chmod x /usr/local/bin/xray
+    cd $(pwd)
 }
 
 # Function to install Trojan Go
 install_trojan_go() {
-    echo -e "=== Installing Trojan Go ==="
-    latest_version=$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)
+    latest_version=$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep tag_name | sed -E 's/.*"v(.*)".*/1/' | head -n 1)
     trojan_go_link="https://github.com/p4gefau1t/trojan-go/releases/download/v${latest_version}/trojan-go-linux-amd64.zip"
     temp_dir=$(mktemp -d)
     cd "$temp_dir"
     curl -sL "$trojan_go_link" -o trojan-go.zip
     unzip -q trojan-go.zip && rm -rf trojan-go.zip
     mv trojan-go /usr/local/bin/trojan-go
-    chmod +x /usr/local/bin/trojan-go
+    chmod x /usr/local/bin/trojan-go
     rm -rf "$temp_dir"
+    cd $(pwd)
 }
 
 # Function to install acme.sh and obtain SSL certificate
 install_acme_and_ssl() {
-    echo -e "=== Installing acme.sh and Obtaining SSL Certificate ==="
+    systemctl stop nginx
     mkdir /root/.acme.sh
     curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
     chmod +x /root/.acme.sh/acme.sh
@@ -59,20 +65,19 @@ install_acme_and_ssl() {
 
 # Function to generate and set a UUID for XRAY configuration files
 generate_and_set_uuid() {
-    echo -e "=== Generating and Setting a UUID for XRAY Configuration Files ==="
     uuid=$(cat /proc/sys/kernel/random/uuid)
-    echo "$uuid" > /usr/local/etc/xray/uuid
+    echo "$uuid" >/usr/local/etc/xray/uuid
 
-    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/vmess-tls.json" | jq '.inbounds[0].settings.clients[0].id = "'$uuid'"' > /usr/local/etc/xray/vmess-tls.json
-    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/vmess-nontls.json" | jq '.inbounds[1].settings.clients[0].id = "'$uuid'"' > /usr/local/etc/xray/vmess-nontls.json
-    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/vless-tls.json" | jq '.inbounds[0].settings.clients[0].id = "'$uuid'"' > /usr/local/etc/xray/vless-tls.json
-    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/vless-nontls.json" | jq '.inbounds[1].settings.clients[0].id = "'$uuid'"' > /usr/local/etc/xray/vless-nontls.json
-    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/trojan-tls.json" | jq '.inbounds[0].settings.clients[0].password = "'$uuid'"' > /usr/local/etc/xray/trojan-tls.json
-    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/trojan-nontls.json" | jq '.inbounds[0].settings.clients[0].password = "'$uuid'"' > /usr/local/etc/xray/trojan-nontls.json
-    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/trojan-tcp.json" | jq '.inbounds[0].settings.clients[0].id = "'$uuid'"' > /usr/local/etc/xray/trojan-tcp.json
-    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/trojan-go.json" | jq '.password[0] = "'$uuid'"' > /usr/local/etc/xray/trojan-go.json
+    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/vmess-tls.json" | jq '.inbounds[0].settings.clients[0].id = "'$uuid'"' >/usr/local/etc/xray/vmess-tls.json
+    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/vmess-nontls.json" | jq '.inbounds[1].settings.clients[0].id = "'$uuid'"' >/usr/local/etc/xray/vmess-nontls.json
+    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/vless-tls.json" | jq '.inbounds[0].settings.clients[0].id = "'$uuid'"' >/usr/local/etc/xray/vless-tls.json
+    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/vless-nontls.json" | jq '.inbounds[1].settings.clients[0].id = "'$uuid'"' >/usr/local/etc/xray/vless-nontls.json
+    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/trojan-tls.json" | jq '.inbounds[0].settings.clients[0].password = "'$uuid'"' >/usr/local/etc/xray/trojan-tls.json
+    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/trojan-nontls.json" | jq '.inbounds[0].settings.clients[0].password = "'$uuid'"' >/usr/local/etc/xray/trojan-nontls.json
+    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/trojan-tcp.json" | jq '.inbounds[0].settings.clients[0].id = "'$uuid'"' >/usr/local/etc/xray/trojan-tcp.json
+    wget -qO - "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/xray/trojan-go.json" | jq '.password[0] = "'$uuid'"' >/usr/local/etc/xray/trojan-go.json
 
-    jq -n '{"vmess": [], "vless": [], "trojan": [], "trojan_tcp": [], "trojan_go": []}' > /usr/local/etc/xray/users.db
+    jq -n '{"vmess": [], "vless": [], "trojan": [], "trojan_tcp": [], "trojan_go": []}' >/usr/local/etc/xray/users.db
 
 }
 
@@ -100,14 +105,15 @@ setup_services_and_configs() {
     # Configure iptables rules
     iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 442 -j ACCEPT
     iptables -I INPUT -m state --state NEW -m udp -p udp --dport 442 -j ACCEPT
-    iptables-save > /etc/iptables.up.rules
-    netfilter-persistent save > /dev/null
-    netfilter-persistent reload > /dev/null
+    iptables-save >/etc/iptables.up.rules
+    netfilter-persistent save >/dev/null
+    netfilter-persistent reload >/dev/null
 }
+
+cd $(pwd)
 
 # Main execution starts here
 domain=$(cat /root/domain)
-email=$(cat /root/email)
 
 echo -e "=== Starting XRAY Core Installation ==="
 sleep 1
