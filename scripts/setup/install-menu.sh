3c9e77db5f78ecf65307eb63f32ef517c572ca7f
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-05 19:16:22 +0700

    Update install-menu.sh

--- scripts/setup/install-menu.sh
+++ scripts/setup/install-menu.sh
@@ -9,3 +9,9 @@
 # Install Speedtest
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 apt-get install speedtest
+
+wget -qO /usr/local/sbin/menu-vmess https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/menu/vmess.sh && chmod +x /usr/local/sbin/menu-vmess
+wget -qO /usr/local/sbin/menu-vless https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/menu/vless.sh && chmod +x /usr/local/sbin/menu-vless
+
+wget -qO /usr/local/sbin/cleaner https://raw.githubusercontent.com/hambosto/MultiVPN/main/tools/cleaner.sh && chmod +x /usr/local/sbin/cleaner
+wget -qO /usr/local/sbin/tcp-bbr https://raw.githubusercontent.com/hambosto/MultiVPN/main/tools/tcp-bbr.sh && chmod +x /usr/local/sbin/tcp-bbr
