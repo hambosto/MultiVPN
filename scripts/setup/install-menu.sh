1aed69e235f148dd29f636a23549c95d9e6db1ae
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-05 15:25:10 +0700

    Update install-menu.sh

--- scripts/setup/install-menu.sh
+++ scripts/setup/install-menu.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 # what are you looking for?
+
+
+
+
+
+# Install Speedtest
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+apt-get install speedtest
