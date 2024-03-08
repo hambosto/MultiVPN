c738cb68052b49bca19f31f24c15031de1599ed6
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-08 23:54:11 +0700

    Update install-ssh.sh

--- scripts/setup/install-ssh.sh
+++ scripts/setup/install-ssh.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-wget -q0 /usr/local/bin/openssh-wss https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/ssh/openssh-wss.py
+wget -qO /usr/local/bin/openssh-wss https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/ssh/openssh-wss.py
 chmod +x /usr/local/bin/openssh-wss
 
 wget -qO /etc/systemd/system/openssh-wss.service https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/services/openssh-wss.service
