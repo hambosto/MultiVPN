107382224ef9b8a88522536a1a3cac547f33c468
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-10 01:39:45 +0700

    Update install-ssh.sh

--- scripts/setup/install-ssh.sh
+++ scripts/setup/install-ssh.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-wget -O /usr/local/bin/proxy3.js https://raw.githubusercontent.com/hambosto/MultiVPN/main/bin/proxy3.js
-chmod +x /usr/local/bin/proxy3.js
+# Function to download and set executable permissions for a script
+download_and_chmod() {
+    local script_url="$1"
+    local destination="$2"
 
+    echo "Downloading $destination..."
+    if wget -qO "$destination" "$script_url"; then
+        echo "Download successful."
+        chmod +x "$destination"
+    else
+        echo "Error: Unable to download the script from $script_url."
+        exit 1
+    fi
+}
 
+# Download and set permissions for proxy3.js
+download_and_chmod "https://raw.githubusercontent.com/hambosto/MultiVPN/main/bin/proxy3.js" "/usr/local/bin/proxy3.js"
 
-# wget -O /usr/local/bin/openssh-websocket https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/ssh/openssh-websocket.py
-# wget -O /usr/local/bin/dropbear-websocket https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/ssh/dropbear-websocket.py
-# wget -O /usr/local/bin/stunnel-websocket https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/ssh/stunnel-websocket.py
+# Download Dropbear WebSocket service file
+dropbear_service_url="https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/services/dropbear-websocket.service"
+dropbear_service_destination="/etc/systemd/system/dropbear-websocket.service"
+download_and_chmod "$dropbear_service_url" "$dropbear_service_destination"
 
-# chmod +x /usr/local/bin/openssh-websocket
-# chmod +x /usr/local/bin/dropbear-websocket
-# chmod +x /usr/local/bin/stunnel-websocket
-
-# wget -qO /etc/systemd/system/openssh-websocket.service https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/services/openssh-websocket.service
-wget -qO /etc/systemd/system/dropbear-websocket.service https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/services/dropbear-websocket.service
-# wget -qO /etc/systemd/system/stunnel-websocket.service https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/services/stunnel-websocket.service
-
+# Reload systemd and restart the Dropbear WebSocket service
 systemctl daemon-reload
-
-# systemctl enable openssh-websocket.service
-# systemctl start openssh-websocket.service
-# systemctl restart openssh-websocket.service
-
 systemctl enable dropbear-websocket.service
 systemctl start dropbear-websocket.service
 systemctl restart dropbear-websocket.service
 
-# systemctl enable stunnel-websocket.service
-# systemctl start stunnel-websocket.service
-# systemctl restart stunnel-websocket.service
+echo "Installation completed successfully!"
