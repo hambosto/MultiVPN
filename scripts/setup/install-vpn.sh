62e7bc1b151686f0411d38ea7f01913cd5a667c9
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-09 16:05:31 +0700

    Update install-vpn.sh

--- scripts/setup/install-vpn.sh
+++ scripts/setup/install-vpn.sh
@@ -74,6 +74,10 @@ configure_ssh() {
     sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
     sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
     sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 50000 -p 109 -p 110 -p 69"/g' /etc/default/dropbear
+
+    wget -q -O /etc/issue.net "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/issue.net" && chmod +x /etc/issue.net
+    echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
+    sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
     
     echo "/bin/false" >> /etc/shells
     echo "/usr/sbin/nologin" >> /etc/shells
