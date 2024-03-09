9a837a6357d58b950c9d1638ed14aae89ba52348
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-09 15:56:03 +0700

    Update install-ssh.sh

--- scripts/setup/install-ssh.sh
+++ scripts/setup/install-ssh.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 wget -qO /usr/local/bin/openssh-wss https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/ssh/openssh-wss.py
+wget -qO /usr/local/bin/dropbear-wss https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/ssh/dropbear-wss.py
+wget -qO /usr/local/bin/stunnel-wss https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/ssh/stunnel-wss.py
+
 chmod +x /usr/local/bin/openssh-wss
+chmod +x /usr/local/bin/dropbear-wss
+chmod +x /usr/local/bin/stunnel-wss
 
 wget -qO /etc/systemd/system/openssh-wss.service https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/services/openssh-wss.service
+wget -qO /etc/systemd/system/dropbear-wss.service https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/services/dropbear-wss.service
+wget -qO /etc/systemd/system/stunnel-wss.service https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/services/stunnel-wss.service
 
 systemctl daemon-reload
+
 systemctl enable openssh-wss
 systemctl restart openssh-wss
+
+systemctl enable dropbear-wss
+systemctl restart dropbear-wss
+
+systemctl enable stunnel-wss
+systemctl restart stunnel-wss
