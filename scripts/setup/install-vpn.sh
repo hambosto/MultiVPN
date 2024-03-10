9829dabe4dfc12fdf2a8065fddb28aaeada6443b
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-10 17:18:27 +0700

    Update install-vpn.sh

--- scripts/setup/install-vpn.sh
+++ scripts/setup/install-vpn.sh
@@ -77,7 +77,7 @@ configure_ssh() {
     sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
     sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
     sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
-    sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 1337"/g' /etc/default/dropbear
+    # sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 1337"/g' /etc/default/dropbear
 
     download_file "https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/issue.net" "/etc/issue.net"
     chmod +x /etc/issue.net
