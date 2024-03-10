addeb0425562a4638617edb7d7c492a8d326b967
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-10 14:54:08 +0700

    Update menu.sh

--- scripts/menu/menu.sh
+++ scripts/menu/menu.sh
@@ -2,8 +2,68 @@
 
 domain=$(cat /usr/local/etc/xray/domain)
 
+get_opsy() {
+    [ -f /etc/redhat-release ] && awk '{print $0}' /etc/redhat-release && return
+    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+    [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
+
+to_kibyte() {
+    local raw=$1
+    awk 'BEGIN{printf "%.0f", '"$raw"' / 1024}'
+}
+
+calc_sum() {
+    local arr=("$@")
+    local s
+    s=0
+    for i in "${arr[@]}"; do
+        s=$((s + i))
+    done
+    echo ${s}
+}
+
+calc_size() {
+    local raw=$1
+    local total_size=0
+    local num=1
+    local unit="KB"
+    if ! [[ ${raw} =~ ^[0-9]+$ ]]; then
+        echo ""
+        return
+    fi
+    if [ "${raw}" -ge 1073741824 ]; then
+        num=1073741824
+        unit="TB"
+    elif [ "${raw}" -ge 1048576 ]; then
+        num=1048576
+        unit="GB"
+    elif [ "${raw}" -ge 1024 ]; then
+        num=1024
+        unit="MB"
+    elif [ "${raw}" -eq 0 ]; then
+        echo "${total_size}"
+        return
+    fi
+    total_size=$(awk 'BEGIN{printf "%.1f", '"$raw"' / '$num'}')
+    echo "${total_size} ${unit}"
+}
+
+_exists() {
+    local cmd="$1"
+    if eval type type >/dev/null 2>&1; then
+        eval type "$cmd" >/dev/null 2>&1
+    elif command >/dev/null 2>&1; then
+        command -v "$cmd" >/dev/null 2>&1
+    else
+        which "$cmd" >/dev/null 2>&1
+    fi
+    local rt=$?
+    return ${rt}
+}
+
 display_banner() {
-    clear
+    # clear
     curl -sS https://raw.githubusercontent.com/hambosto/MultiVPN/main/config/banner
 }
 
@@ -83,7 +143,7 @@ restart() {
 setup_dns() {
     clear
     echo "Setting up $1"
-    
+
     cat > /etc/systemd/resolved.conf << END
 [Resolve]
 DNS=$2
@@ -140,18 +200,18 @@ change_dns() {
     read -p "Select Option [1-11]: " dns
 
     case $dns in
-    1) setup_dns "Google DNS" "8.8.8.8 8.8.4.4" ;;
-    2) setup_dns "Cloudflare DNS" "1.1.1.1 1.0.0.1" ;;
-    3) setup_dns "Cisco OpenDNS" "208.67.222.222 208.67.222.220" ;;
-    4) setup_dns "Quad9 DNS" "9.9.9.9 149.112.112.112" ;;
-    5) setup_dns "Level 3 DNS" "4.2.2.1 4.2.2.2" ;;
-    6) setup_dns "Freenom World DNS" "80.80.80.80 80.80.81.81" ;;
-    7) setup_dns "Neustar DNS" "156.154.70.2 156.154.71.2" ;;
-    8) setup_dns "AdGuard DNS" "94.140.14.14 94.140.15.15" ;;
-    9) setup_dns "Control D DNS" "76.76.2.43 76.76.10.43" ;;
-    10) setup_custom_dns ;;
-    11) menu ;;
-    *) echo "Invalid option. Please enter a number between 1 and 11." ;;
+        1) setup_dns "Google DNS" "8.8.8.8 8.8.4.4" ;;
+        2) setup_dns "Cloudflare DNS" "1.1.1.1 1.0.0.1" ;;
+        3) setup_dns "Cisco OpenDNS" "208.67.222.222 208.67.222.220" ;;
+        4) setup_dns "Quad9 DNS" "9.9.9.9 149.112.112.112" ;;
+        5) setup_dns "Level 3 DNS" "4.2.2.1 4.2.2.2" ;;
+        6) setup_dns "Freenom World DNS" "80.80.80.80 80.80.81.81" ;;
+        7) setup_dns "Neustar DNS" "156.154.70.2 156.154.71.2" ;;
+        8) setup_dns "AdGuard DNS" "94.140.14.14 94.140.15.15" ;;
+        9) setup_dns "Control D DNS" "76.76.2.43 76.76.10.43" ;;
+        10) setup_custom_dns ;;
+        11) menu ;;
+        *) echo "Invalid option. Please enter a number between 1 and 11." ;;
     esac
 }
 
@@ -227,13 +287,78 @@ get_status() {
     fi
 }
 
-# Get service statuses
 status_nginx=$(get_status "nginx")
 status_xray=$(get_status "xray")
 status_ssh=$(get_status "dropbear-websocket")
-# status_trojan_go=$(get_status "trojan-go")
 
 ip_address=$(curl -s icanhazip.com/ip)
+
+cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')
+cores=$(awk -F: '/^processor/ {core++} END {print core}' /proc/cpuinfo)
+freq=$(awk -F'[ :]' '/cpu MHz/ {print $4;exit}' /proc/cpuinfo)
+ccache=$(awk -F: '/cache size/ {cache=$2} END {print cache}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')
+
+tram=$(
+    LANG=C
+    free | awk '/Mem/ {print $2}'
+)
+tram=$(calc_size "$tram")
+uram=$(
+    LANG=C
+    free | awk '/Mem/ {print $3}'
+)
+uram=$(calc_size "$uram")
+
+in_kernel_no_swap_total_size=$(
+    LANG=C
+    df -t simfs -t ext2 -t ext3 -t ext4 -t btrfs -t xfs -t vfat -t ntfs --total 2>/dev/null | grep total | awk '{ print $2 }'
+)
+swap_total_size=$(free -k | grep Swap | awk '{print $2}')
+zfs_total_size=$(to_kibyte "$(calc_sum "$(zpool list -o size -Hp 2> /dev/null)")")
+disk_total_size=$(calc_size $((swap_total_size + in_kernel_no_swap_total_size + zfs_total_size)))
+in_kernel_no_swap_used_size=$(
+    LANG=C
+    df -t simfs -t ext2 -t ext3 -t ext4 -t btrfs -t xfs -t vfat -t ntfs --total 2>/dev/null | grep total | awk '{ print $3 }'
+)
+swap_used_size=$(free -k | grep Swap | awk '{print $3}')
+zfs_used_size=$(to_kibyte "$(calc_sum "$(zpool list -o allocated -Hp 2> /dev/null)")")
+disk_used_size=$(calc_size $((swap_used_size + in_kernel_no_swap_used_size + zfs_used_size)))
+
+swap=$(
+    LANG=C
+    free | awk '/Swap/ {print $2}'
+)
+swap=$(calc_size "$swap")
+uswap=$(
+    LANG=C
+    free | awk '/Swap/ {print $3}'
+)
+uswap=$(calc_size "$uswap")
+
+opsy=$(get_opsy)
+arch=$(uname -m)
+if _exists "getconf"; then
+    lbit=$(getconf LONG_BIT)
+else
+    echo "${arch}" | grep -q "64" && lbit="64" || lbit="32"
+fi
+kern=$(uname -r)
+
+up=$(awk '{a=$1/86400;b=($1%86400)/3600;c=($1%3600)/60} {printf("%d days, %d hour %d min\n",a,b,c)}' /proc/uptime)
+if _exists "w"; then
+    load=$(
+        LANG=C
+        w | head -1 | awk -F'load average:' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+    )
+elif _exists "uptime"; then
+    load=$(
+        LANG=C
+        uptime | head -1 | awk -F'load average:' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+    )
+fi
+
+
+date_today=$(date -R | cut -d " " -f -4)
 
 today_download="$(vnstat | grep today | awk 'NR==1{print $2" "substr($3, 1, 3)}')"
 today_upload="$(vnstat | grep today | awk 'NR==1{print $5" "substr($6, 1, 3)}')"
@@ -252,36 +377,66 @@ region="$(wget -q -T10 -O- ipinfo.io/region)"
 display_banner
 
 echo -e "------------------------------------------------------------------------------------------------"
-echo -e "Domain       : $domain"
-echo -e "IP Address   : $ip_address"
+echo -e "Date          : $date_today"
+echo -e "Domain        : $domain"
+
+if [ -n "$cname" ]; then
+    echo -e "CPU Model     : $cname"
+else
+    echo -e "CPU Model     : CPU model not detected"
+fi
+if [ -n "$freq" ]; then
+    echo -e "CPU Cores     : $cores @ $freq MHz"
+else
+    echo -e "CPU Cores     : $cores"
+fi
+if [ -n "$ccache" ]; then
+    echo -e "CPU Cache     : $ccache"
+fi
+
+echo -e "Total Disk    : $disk_total_size ($disk_used_size Used)"
+echo -e "Total Memory  : $tram ($uram Used)"
+
+if [ "$swap" != "0" ]; then
+    echo -e "Total Swap   : $swap ($uswap Used)"
+fi
+
+echo -e "System uptime : $up"
+echo -e "Load average  : $load"
+
+echo -e "OS            : $opsy"
+echo -e "Arch          : $arch ($lbit Bit)"
+echo -e "Kernel        : $kern"
+
+echo -e "IP Address    : $ip_address"
 
 if [[ -n "${org}" ]]; then
-    echo -e "Organization : $org"
+    echo -e "Organization  : $org"
 fi
 if [[ -n "${city}" && -n "${country}" ]]; then
-    echo -e "Location     : $city / $country"
+    echo -e "Location      : $city / $country"
 fi
 if [[ -n "${region}" ]]; then
-    echo -e "Region       : $region"
+    echo -e "Region        : $region"
 fi
 if [[ -z "${org}" ]]; then
-    echo -e "Region       : No ISP detected"
+    echo -e "Region        : No ISP detected"
 fi
 echo -e "------------------------------------------------------------------------------------------------"
 echo -e "Daily Bandwidth:"
-echo -e "↑↑ Upload    : $today_upload"
-echo -e "↓↓ Download  : $today_download"
-echo -e "≈≈ Total     : $today_total"
+echo -e "↑↑ Upload     : $today_upload"
+echo -e "↓↓ Download   : $today_download"
+echo -e "≈≈ Total      : $today_total"
 echo -e "------------------------------------------------------------------------------------------------"
 echo -e "Montly Bandwidth:"
-echo -e "↑↑ Upload    : $month_upload"
-echo -e "↓↓ Download  : $month_download"
-echo -e "≈≈ Total     : $month_total"
+echo -e "↑↑ Upload     : $month_upload"
+echo -e "↓↓ Download   : $month_download"
+echo -e "≈≈ Total      : $month_total"
 echo -e "------------------------------------------------------------------------------------------------"
 echo -e "VPN Service:"
-echo -e "SSH WS       : $status_ssh"
-echo -e "Nginx        : $status_nginx"
-echo -e "V2Ray        : $status_xray"
+echo -e "SSH WS        : $status_ssh"
+echo -e "Nginx         : $status_nginx"
+echo -e "V2Ray         : $status_xray"
 echo -e "------------------------------------------------------------------------------------------------"
 echo -e "Menu Options:"
 echo -e "1. SSH Websocket       5. Restart VPN Service   9.  Speedtest VPS"
@@ -296,61 +451,61 @@ read -p "Select menu: " menu
 
 # Case statement for menu options
 case $menu in
-1)
-    clear
-    menu-ssh # for ssh, later.
-    ;;
-2)
-    clear
-    menu-vmess
-    ;;
-3)
-    clear
-    menu-vless
-    ;;
-4)
-    clear
-    menu-trojan
-    ;;
-5)
-    clear
-    restart
-    ;;
-6)
-    clear
-    change_domain
-    ;;
-7)
-    clear
-    renew_ssl
-    ;;
-8)
-    clear
-    change_dns
-    ;;
-9)
-    clear
-    speedtest
-    ;;
-10)
-    clear
-    tcp-bbr
-    ;;
-11)
-    clear
-    status
-    ;;
-12)
-    clear
-    bash <(curl -L -s check.unlock.media) -E
-    ;;
-13)
-    clear
-    exit
-    ;;
-*)
-    clear
-    menu
-    ;;
+    1)
+        clear
+        menu-ssh # for ssh, later.
+        ;;
+    2)
+        clear
+        menu-vmess
+        ;;
+    3)
+        clear
+        menu-vless
+        ;;
+    4)
+        clear
+        menu-trojan
+        ;;
+    5)
+        clear
+        restart
+        ;;
+    6)
+        clear
+        change_domain
+        ;;
+    7)
+        clear
+        renew_ssl
+        ;;
+    8)
+        clear
+        change_dns
+        ;;
+    9)
+        clear
+        speedtest
+        ;;
+    10)
+        clear
+        tcp-bbr
+        ;;
+    11)
+        clear
+        status
+        ;;
+    12)
+        clear
+        bash <(curl -L -s check.unlock.media) -E
+        ;;
+    13)
+        clear
+        exit
+        ;;
+    *)
+        clear
+        menu
+        ;;
 esac
 
