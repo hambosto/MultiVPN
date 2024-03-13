3e19c3bbd5b87f7b64bd49ad89d66a2c3fb1a90f
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-13 18:50:33 +0700

    Update install-menu.sh

--- scripts/setup/install-menu.sh
+++ scripts/setup/install-menu.sh
@@ -6,16 +6,6 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Install Speedtest if not already installed
-if ! command -v speedtest &> /dev/null; then
-    echo "Installing Speedtest..."
-    curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-    apt-get install speedtest -y
-    echo "Speedtest installed."
-else
-    echo "Speedtest is already installed."
-fi
-
 # Download and install menu scripts
 echo "Downloading and installing menu scripts..."
 wget -qO /usr/local/sbin/menu https://raw.githubusercontent.com/hambosto/MultiVPN/main/scripts/menu/menu.sh && chmod +x /usr/local/sbin/menu
@@ -28,6 +18,7 @@ echo "Downloading and installing utility scripts..."
 wget -qO /usr/local/sbin/cleaner https://raw.githubusercontent.com/hambosto/MultiVPN/main/tools/cleaner.sh && chmod +x /usr/local/sbin/cleaner
 wget -qO /usr/local/sbin/bbr https://raw.githubusercontent.com/hambosto/MultiVPN/main/tools/bbr.sh && chmod +x /usr/local/sbin/bbr
 wget -qO /usr/local/sbin/expiry https://raw.githubusercontent.com/hambosto/MultiVPN/main/tools/expiry.sh && chmod +x /usr/local/sbin/expiry
+wget -qO /usr/local/sbin/speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py && /usr/local/sbin/speedtest-cli
 echo "Utility scripts installed."
 
 echo "Setup completed successfully."
