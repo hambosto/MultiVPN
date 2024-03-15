4f3c16a56c168b1c8c1aad338129f8da4bc6cb07
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2024-03-15 14:39:41 +0700

    Update expiry.sh

--- tools/expiry.sh
+++ tools/expiry.sh
@@ -3,28 +3,37 @@
 users_file="/usr/local/etc/xray/users.db"
 xray_config="/usr/local/etc/xray/config.json"
 
-current_date=$(date "+%Y-%m-%d")
+current_date=$(date " %Y-%m-%d")
 
 remove_expired_users() {
     local user_type="$1"
     local filtered_data="$(jq --arg current_date "$current_date" --arg user_type "$user_type" '.[$user_type] | map(select(.expiry == $current_date))' "$users_file")"
 
     if [ -n "$filtered_data" ]; then
-        echo $(jq --arg current_date "$current_date" --arg user_type "$user_type" '.[$user_type] |= map(select(.expiry != $current_date))' "$users_file") > "$users_file"
+        echo $(jq --arg current_date "$current_date" --arg user_type "$user_type" '.[$user_type] |= map(select(.expiry != $current_date))' "$users_file") >"$users_file"
 
         local username="$(echo "$filtered_data" | jq -r '.[0].user')"
         local start_index=1
         local end_index=0
-        
+
         case "$user_type" in
-            "vmess") end_index=2 ;;
-            "vless") start_index=3; end_index=4 ;;
-            "trojan") start_index=5; end_index=6 ;;
+        "vmess")
+            end_index=2
+            rm -f "/usr/local/etc/xray/vmess-$username-tls.json" "/usr/local/etc/xray/vmess-$username-nonetls.json"
+            ;;
+        "vless")
+            start_index=3
+            end_index=4
+            ;;
+        "trojan")
+            start_index=5
+            end_index=6
+            ;;
         esac
 
         # Loop through the inbounds sections
-        for ((i=start_index; i<=end_index; i++)); do
-            echo $(jq --arg username "$username" --argjson index "$i" '.inbounds[$index].settings.clients = (.inbounds[$index].settings.clients | map(select(.email != $username)))' "$xray_config") > "$xray_config"
+        for ((i = start_index; i <= end_index; i)); do
+            echo $(jq --arg username "$username" --argjson index "$i" '.inbounds[$index].settings.clients = (.inbounds[$index].settings.clients | map(select(.email != $username)))' "$xray_config") >"$xray_config"
         done
         systemctl restart xray.service
     fi
